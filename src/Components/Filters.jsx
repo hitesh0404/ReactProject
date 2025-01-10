058f9c33b2deed1ff6ac76e600d3f70b44f6662e
hitesh <hiteshkukaramprajapati@gmail.com>
2025-01-10 08:52:21 +0530

    Filter with limited Products Rendering

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -1,45 +1,50 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import ProductList from './ProductList';
-function Filters(props) {
-    const [lth,setLTH] = useState(false);
-    const [htl,setHTL] = useState(false);
-    return (
-        <>
-        <div className='container'>
-            <div className='row'>
-                <div className="col-3 col-sm-4 col-md-2 col-xl-1">
-                    <div className="row">
-                        <div className="col-12">
-                            <ul>
-                                <li><input type="radio" 
-                                   checked={lth}
-                                     name="price" id="" onClick={()=>{
-                                    setLTH(true)
-                                    setHTL(false)
-                                    }
-                                } />Low to High</li>
-                                <li><input type="radio" 
-                                 checked={htl}
-                                name="price" id="" onClick={()=>{
-                                    setHTL(true)
-                                    setLTH(false)
-                                    }
-                                }/>High to Low</li> 
-                            </ul>
-                        </div>
-                        <div className="col-12">
-                            
-                            </div>
-                    </div>
-                </div>
-                <div className="col-9">
-                   { lth ? ( <ProductList lth />) : ( <ProductList htl />) }
-                </div>
-            </div>
+
+function Filters() {
+  const [lth, setLTH] = useState(false);
+  const [htl, setHTL] = useState(false);
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <ul>
+              <li>
+                <input
+                  type="radio"
+                  checked={lth}
+                  name="price"
+                  onClick={() => {
+                    setLTH(true);
+                    setHTL(false);
+                  }}
+                />
+                Low to High
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  checked={htl}
+                  name="price"
+                  onClick={() => {
+                    setHTL(true);
+                    setLTH(false);
+                  }}
+                />
+                High to Low
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-9">
+            <ProductList lth={lth} htl={htl} />
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Filters;
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,71 +1,89 @@
 import React, { useEffect, useState } from 'react';
 
 function ProductList(props) {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState([]);
-  const [isLoading,setIsLoading] = useState(false);
-  const [isError,setIsError] = useState(false);
-  console.log(props);
-  async function  doSomething() {
+  let { lth, htl } = props; // Destructure the props passed for filtering
+  const [count, setCount] = useState(4); // Initial count for the first set of products
+  const [data, setData] = useState([]); // Only one state variable for products
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  async function fetchProducts() {
     setIsLoading(true);
-   await fetch('https://fakestoreapi.com/products/')
-      .then(res => res.json())
-      .then(json => setData(json)) 
-      .catch(err => setIsError('Error fetching data:'+ err)); // Catch errors
-    setIsLoading(false);
-   
+    if(data.length<=1){
+    try {
+      const res = await fetch('https://fakestoreapi.com/products/');
+      setProducts(await res.json());
+    } catch (err) {
+      setIsError('Error fetching data: ' + err);
+    }
+  }
+      setData(products.slice(0, count)); // Display only 'count' number of products
+      if (lth) {
+        data.sort((a, b) => a.price - b.price);     
+      } else if (htl) {
+        data.sort((a, b) => b.price - a.price);    
+      }      
+      setIsLoading(false);
   }
 
   useEffect(() => {
-    doSomething();  
-   
-  }, [count,]);  
+    fetchProducts();
+  }, [data,count, lth, htl]);  // Re-fetch when count, lth, or htl changes
 
-  if(isLoading)
-    return (<div >
-      <div
-        class="d-flex justify-content-center align-items-center"
-      >
-        <div
-          className="spinner-border text-primary spinner-border-sm"
-          role="status"
-          style={{height:"100px", width:"100px", borderWidth :"20px",marginTop:"30%"}}
-        >
-          <span class="visually-hidden">Loading...</span>
+  if (isLoading)
+    return (
+      <div>
+        <div className="d-flex justify-content-center align-items-center">
+          <div
+            className="spinner-border text-primary spinner-border-sm"
+            role="status"
+            style={{ height: '100px', width: '100px', borderWidth: '20px', marginTop: '30%' }}
+          >
+            <span className="visually-hidden">Loading...</span>
+          </div>
         </div>
+        Loading...
       </div>
-      Loading...</div>)
+    );
 
-  if(isError)
-    return (<div>{isError}</div>)
+  if (isError) return <div>{isError}</div>;
 
   return (
     <div>
-    <h1>All Product List (Count: {count})</h1>
-    <button 
-      type="button" 
-      onClick={() => {
-        setCount(prevCount => prevCount + 3);  // Increase count by 3 in one update
-      }}
-    >
-      Click here to increase
-    </button>
-    
-      <div className='col-12'>
+      <h1>Product List (Count: {count})</h1>
+
+      <div className="col-12">
         <div className="row">
           {data.map((product, index) => (
-            <div className='col-3'>
-            <div className="card text-white bg-primary">
-              <img className="card-img-top" src={product.image} alt="Title" />
-              <div className="card-body">
-                <h4 className="card-title" style={{color:"black"}} >{product.title}</h4>
-                <p className="card-text">{product.description.split(' ').slice(0,10)+'...'}</p>
-              </div>
+            <div className="col-3" key={index}>
+              <div className="card text-white bg-primary">
+                <img className="card-img-top" src={product.image} alt="Title" />
+                <div className="card-body">
+                  <h4 className="card-title" style={{ color: 'black' }}>
+                    {product.title}
+                  </h4>
+                  <p className="card-text">{product.description.split(' ').slice(0, 10).join(' ')}...</p>
+                  <p className="card-text">Price: ${product.price}</p>
+                </div>
               </div>
             </div>
           ))}
         </div>
       </div>
+
+      <button
+        type="button"
+        onClick={() => {
+          if(count<=(products.length-4))               
+          setCount((prevCount) => prevCount + 4); 
+          else 
+            if(count<products.length)
+              setCount((products.length)) 
+        }}
+      >
+        Load More
+      </button>
     </div>
   );
 }
